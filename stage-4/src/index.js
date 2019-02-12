75770c110a3ab231cd8858bc21783b586f4fdf67
shauntoconnell <shauntoconnell@gmail.com>
2019-02-12 15:59:52 -0700

    javascript-5-afternoon complete

--- stage-4/src/index.js
+++ stage-4/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
 ReactDOM.render(
-
+	<HashRouter>
     <Provider store={store}>
-        <App />
+      <App />
     </Provider>
-    , document.getElementById('root'));
+	</HashRouter>
+  , document.getElementById('root'));
 registerServiceWorker();
